bed99e0307f9d3abf70588a8645ddbc61d4de5b6
udhajare <udayhajare18@gmail.com>
2022-06-09 22:20:33 +0530

    Initial Structure

---
+++ src/main/java/com/trade/management/StmStockTradeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.trade.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StmStockTradeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StmStockTradeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade/management/StmStockTradeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StmStockTradeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
